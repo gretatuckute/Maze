a7deccebe026a4ac47f7737a8907e9860c432f81
Greta Tuckute <gretatu@mit.edu>
2023-01-06 14:37:52 -0500

    run punc test

--- maze_automate/1_format_materials.py
+++ maze_automate/1_format_materials.py
@@ -15,6 +15,7 @@ SAVEDIR = os.getcwd()+'/input_files/'
 
 if __name__ == "__main__":
     save = True
+    generate_punc_test = True # generate a test set with/without punctuation
 
     #### LOAD THE ORIGINAL STIMSET ####
     fname_stimset = 'beta-control-neural_stimset_D-S_light'
@@ -28,15 +29,39 @@ if __name__ == "__main__":
     assert(df_stimset.item_id.max() == 2000)
     assert(df_stimset.sentence.nunique() == 2000)
 
+    ## Change all semi-colons to colon (as this is the delimiter)
+    df_stimset.sentence = df_stimset.sentence.str.replace(';', ':')
+
     #### FORMAT THE STIMSET ####
-    col1_cond_label = 'crit_item' # not a test
+    col1_cond_label = 'crit_item'
+
+    if generate_punc_test:
+        # Sample 10 items
+        df_stimset = df_stimset.query('cond == "D"').sample(50, random_state=1)
 
     # Write all the sentences to a text file, newlines, and save
     if save:
-        with open(f'{SAVEDIR}/{fname_stimset}.txt', 'w') as f:
-            for row in df_stimset.itertuples():
-                f.write(f'{col1_cond_label};{row.item_id};{row.sentence}'+'\n')
-        f.close()
+        if generate_punc_test:
+            df_stimset['sentence_no_punc'] = df_stimset.sentence.str.replace('[^\w\s]','')
+
+            # version with and without punctuation
+            with open(f'{SAVEDIR}/{fname_stimset}_truncated-w-punc.txt', 'w') as f:
+                for row in df_stimset.itertuples():
+                    f.write(f'{col1_cond_label};{row.item_id};{row.sentence}' + '\n')
+            f.close()
+
+            with open(f'{SAVEDIR}/{fname_stimset}_truncated-no-punc.txt', 'w') as f:
+                for row in df_stimset.itertuples():
+                    f.write(f'{col1_cond_label};{row.item_id};{row.sentence_no_punc}' + '\n')
+            f.close()
+
+
+
+        else:
+            with open(f'{SAVEDIR}/{fname_stimset}.txt', 'w') as f:
+                for row in df_stimset.itertuples():
+                    f.write(f'{col1_cond_label};{row.item_id};{row.sentence}'+'\n')
+            f.close()
 
     # Save a version of the original stimset used
     df_stimset.to_csv(f'{SAVEDIR}/{fname_stimset}_original.csv', index=False)
